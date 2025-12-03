5cc7088ce4a750c0dfc4b28a0b82024594c59f0f
zizoumunoz <zmunoz@outlook.es>
2025-12-02 19:26:50 -0800

    Add logic for changing cursor on text box hover

--- Components/TextInput.cs
+++ Components/TextInput.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Raylib_cs;
 
 namespace VGP133_Final_Assignment.Components
 {
@@ -8,18 +9,49 @@ namespace VGP133_Final_Assignment.Components
     {
         public TextInput(int length, int height, int xCoord, int yCoord)
         {
-            _length = length;
+            _width = length;
             _height = height;
             _xCoord = xCoord;
             _yCoord = yCoord;
+            _box = new Rectangle(_xCoord, _yCoord, _width, _height);
         }
 
-        private int _length;
+        public void Update()
+        {
+            if (Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), _box))
+            {
+                _isMouseOnText = true;
+            }
+            else
+            {
+                _isMouseOnText = false;
+            }
+
+            if (_isMouseOnText)
+            {
+                Raylib.SetMouseCursor(MouseCursor.IBeam);
+            }
+            else
+            {
+                Raylib.SetMouseCursor(MouseCursor.Arrow);
+            }
+        }
+
+        public void Render()
+        {
+
+            Raylib.DrawRectangleRec(_box, Color.Red);
+        }
+
+        private int _width;
         private int _height;
         private int _xCoord;
         private int _yCoord;
         private string? _textData;
-        public int Length { get => _length; set => _length = value; }
+        private bool _isMouseOnText;
+        private Rectangle _box;
+
+        public int Width { get => _width; set => _width = value; }
         public int Height { get => _height; set => _height = value; }
         public int XCoord { get => _xCoord; set => _xCoord = value; }
         public int YCoord { get => _yCoord; set => _yCoord = value; }
