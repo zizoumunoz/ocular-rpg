b0ac85ff5cc34733e74452979c2a35e0bedf6e1e
zizoumunoz <zmunoz@outlook.es>
2025-12-02 21:22:03 -0800

    Implement text box

--- Scenes/CharacterCreation.cs
+++ Scenes/CharacterCreation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Raylib_cs;
+using VGP133_Final_Assignment.Components;
 using VGP133_Final_Assignment.Core;
 
 namespace VGP133_Final_Assignment.Scenes
@@ -14,13 +15,16 @@ namespace VGP133_Final_Assignment.Scenes
         }
         public override void Update()
         {
-            
+            _nameBox.Update();
         }
         public override void Render()
         {
             Raylib.ClearBackground(Color.RayWhite);
-            Raylib.DrawText("Character Creation Scene", 200, 200, 20, Color.DarkGray);
+            Raylib.DrawText("Character Creation Scene", 0, 0, 20, Color.DarkGray);
+            _nameBox.Render();
         }
+
+        private TextInput _nameBox = new TextInput(700, 50, 200, 250);
 
     }
 }
