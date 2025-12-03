9d2be4960e14688dc31471fff013812ddc7b02ac
zizoumunoz <zmunoz@outlook.es>
2025-12-03 14:52:46 -0800

    Add ButtonCircle class

---
+++ Components/ButtonCircle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib_cs;
+
+namespace VGP133_Final_Assignment.Components
+{
+    public class ButtonCircle
+    {
+        public ButtonCircle(float radius, int xCoordinate, int yCoordinate)
+        {
+            _radius = radius;
+            _xCoordinate = xCoordinate;
+            _yCoordinate = yCoordinate;
+        }
+
+        
+
+
+        private float _radius;
+        private int _xCoordinate;
+        private int _yCoordinate;
+
+        public float Radius { get => _radius; set => _radius = value; }
+        public int XCoordinate { get => _xCoordinate; set => _xCoordinate = value; }
+        public int YCoordinate { get => _yCoordinate; set => _yCoordinate = value; }
+    }
+}
