a68e3a5741d555c10566bd0e54ed825f6960b748
zizoumunoz <zmunoz@outlook.es>
2025-12-03 14:42:39 -0800

    Add docstring

--- Scenes/CharacterCreation.cs
+++ Scenes/CharacterCreation.cs
@@ -9,22 +9,29 @@ namespace VGP133_Final_Assignment.Scenes
 {
     public class CharacterCreation : Scene
     {
+        /// <summary>
+        /// Initialize CharacterCreation Scene instance.
+        /// </summary>
+        /// <param name="sceneHandler">Main scene handler</param>
         public CharacterCreation(SceneHandler sceneHandler) : base(sceneHandler)
         {
             SceneName = "Character Creation";
         }
+
         public override void Update()
         {
             _nameBox.Update();
         }
+
         public override void Render()
         {
             Raylib.ClearBackground(Color.RayWhite);
-            Raylib.DrawText("Character Creation Scene", 0, 0, 20, Color.DarkGray);
+            _background.Render();
+            Raylib.DrawText("Character Creation Scene", 0, 0, 20, Color.Black);
             _nameBox.Render();
         }
 
-        private TextInput _nameBox = new TextInput(700, 50, 200, 250);
+        private TextInput _nameBox = new TextInput(540, 51, 248, 112);
 
     }
 }
