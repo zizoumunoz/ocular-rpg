98905dcfac5475f308e9314681da3b94fd35bd9d
Zinadine Muñoz Luna <zmunoz@outlook.es>
2025-12-05 14:11:02 -0800

    Add WorldMap scene

---
+++ Scenes/WorldMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VGP133_Final_Assignment.Core;
+
+namespace VGP133_Final_Assignment.Scenes
+{
+    public class WorldMap : Scene
+    {
+        public WorldMap(SceneHandler sceneHandler) : base(sceneHandler)
+        {
+        }
+
+        public override void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
