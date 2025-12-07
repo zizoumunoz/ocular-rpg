938368556859bcdc8b0a6f7b7f535c7f22f1fd2a
zizoumunoz <zmunoz@outlook.es>
2025-12-07 15:59:50 -0800

    Add ResolutionManager

---
+++ Core/ResolutionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VGP133_Final_Assignment.Core
+{
+    public static class ResolutionManager
+    {
+        public static void SetResolution()
+        {
+
+        }
+
+        private static int s_virtualWidth = 384;
+        private static int s_virtualHeight = 216;
+
+        private static int s_uiScale = 5;
+    }
+}
