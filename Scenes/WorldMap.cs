841d26ca3365d0049bcd51ac015e5be0a2fee9c3
zizoumunoz <zmunoz@outlook.es>
2025-12-07 15:08:19 -0800

    Add sprites to WorldMap scene

--- Scenes/WorldMap.cs
+++ Scenes/WorldMap.cs
@@ -1,5 +1,7 @@
-﻿using VGP133_Final_Assignment.Core;
+﻿using System.Numerics;
+using VGP133_Final_Assignment.Core;
 using Raylib_cs;
+using VGP133_Final_Assignment.Components;
 
 namespace VGP133_Final_Assignment.Scenes
 {
@@ -12,7 +14,13 @@ namespace VGP133_Final_Assignment.Scenes
         public override void Render()
         {
             Raylib.ClearBackground(Color.RayWhite);
-            Raylib.DrawText("WorldMap Scene", 0, 0, 20, Color.Black);
+
+            _background.Render();
+            _borders.Render();
+            _buttons.Render();
+            _statusWindows.Render();
+
+            Raylib.DrawText("WorldMap Scene", 0, 0, 20, Color.RayWhite);
         }
 
         public override void Update()
@@ -20,9 +28,18 @@ namespace VGP133_Final_Assignment.Scenes
 
         }
 
+        private const int _uiScale = 5;
+
         // Sprites
 
-
+        Sprite _background =
+            new Sprite("world_background", new Vector2(0, 0), _uiScale);
+        Sprite _borders =
+            new Sprite("world_borders", new Vector2(0, 0), _uiScale);
+        Sprite _buttons =
+            new Sprite("world_buttons", new Vector2(0, 0), _uiScale);
+        Sprite _statusWindows =
+            new Sprite("world_status_window", new Vector2(0, 0), _uiScale);
 
     }
 }
