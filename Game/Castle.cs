e30647479494111c73bd67e8e7a942203b17e549
zizoumunoz <zmunoz@outlook.es>
2025-12-10 16:39:47 -0800

    Add Castle Terrain class

---
+++ Game/Castle.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using VGP133_Final_Assignment.Components;
+
+namespace VGP133_Final_Assignment.Game
+{
+    public class Castle : Terrain
+    {
+        public Castle(Vector2 location, List<Monster>? monsterPool) : base(location, monsterPool)
+        {
+            _name = "Castle";
+            _location = location;
+            _sprite = new Sprite("terrain_boss", _location);
+        }
+
+        public override void Render()
+        {
+            _sprite.Render();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
