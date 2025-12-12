9d64e1d42e31940af279f08a7361ff573be5cb6d
Zinadine Muñoz Luna <zmunoz@outlook.es>
2025-12-12 13:25:33 -0800

    Change text class to use vector2 for location

--- Components/Text.cs
+++ Components/Text.cs
@@ -1,22 +1,22 @@
 ﻿using VGP133_Final_Assignment.Interfaces;
 using Raylib_cs;
+using System.Numerics;
 
 namespace VGP133_Final_Assignment.Components
 {
     public class Text : IDrawable
     {
-        public Text(string textData, float x, float y, int fontSize, Color color)
+        public Text(string textData, Vector2 position, int fontSize, Color color)
         {
             _textData = textData;
-            _x = x;
-            _y = y;
-            _fontSize = fontSize;
+            _position = position;
+            _fontSize = fontSize; 
             _color = color;
         }
 
         public void Render()
         {
-            Raylib.DrawText(_textData, (int)_x, (int)_y, _fontSize, _color);
+            Raylib.DrawText(_textData, (int)_position.X, (int)_position.Y, _fontSize, _color);
         }
 
         public void Update()
@@ -25,17 +25,16 @@ namespace VGP133_Final_Assignment.Components
         }
 
         private string _textData = "";
-        private float _x;
-        private float _y;
+        private Vector2 _position;
         private int _fontSize;
         private Color _color;
         private bool _isVisible;
 
 
         public string TextData { get => _textData; set => _textData = value; }
-        public float X { get => _x; set => _x = value; }
-        public float Y { get => _y; set => _y = value; }
+        
         public Color Color { get => _color; set => _color = value; }
         public bool IsVisible { get => _isVisible; set => _isVisible = value; }
+        public Vector2 Position { get => _position; set => _position = value; }
     }
 }
