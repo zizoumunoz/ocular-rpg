b9c1c7eb6059dbf72bd6dee10ed22357ab559664
zizoumunoz <zmunoz@outlook.es>
2025-12-14 19:09:42 -0800

    Add GameColor utility class

---
+++ Game/GameColors.cs
@@ -0,0 +1,10 @@
+﻿using Raylib_cs;
+
+namespace VGP133_Final_Assignment.Game
+{
+    public static class GameColors
+    {
+        public static readonly Color DarkBrown = new Color(178, 139, 120);
+        public static readonly Color LightBrown = new Color(246, 216, 150);
+    }
+}
