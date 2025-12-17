c51980a3cd2f022eabce79ea91f0c50ad59e11bb
zizoumunoz <zmunoz@outlook.es>
2025-12-17 01:21:20 -0800

    Add inventory class

---
+++ Game/Inventory.cs
@@ -0,0 +1,26 @@
+﻿namespace VGP133_Final_Assignment.Game
+{
+    public class Inventory
+    {
+        public void AddItem(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public void SortQuantity()
+        {
+
+        }
+
+        public void SortName()
+        {
+
+        }
+
+        public void SortType()
+        {
+
+        }
+        private List<Item> _items = new List<Item>();
+    }
+}
