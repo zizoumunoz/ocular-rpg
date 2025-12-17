312fee27f80802fcc74551b66a5e2d1dcbfb888a
zizoumunoz <zmunoz@outlook.es>
2025-12-16 22:33:16 -0800

    Add Item class

---
+++ Game/Item.cs
@@ -0,0 +1,32 @@
+﻿
+namespace VGP133_Final_Assignment.Game
+{
+    public class Item
+    {
+        public Item(string name, string type, int quantity, int value)
+        {
+            _name = name;
+            _type = type;
+            _value = value;
+            Quantity = quantity;
+        }
+
+        private string _name;
+        private string _type;
+        private int _quantity;
+        private int _value;
+
+        public string Name { get => _name; }
+        public string Type { get => _type; }
+        public int Value { get => _value; }
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (value <= 0) _quantity = 0;
+                else _quantity = value;
+            }
+        }
+    }
+}
